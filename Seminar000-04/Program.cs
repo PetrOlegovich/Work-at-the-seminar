3d6c554d4a5ee3aec99b64238672429118829f62
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-13 21:29:29 +0300

    Поиск последней цифры трехзначного число)

--- Seminar000-04/Program.cs
+++ Seminar000-04/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("введите трехзначное число : ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (num>999 || num<100)
+{
+    Console.WriteLine("Это не трехзначное число");
+}
+else
+{
+   Console.WriteLine(num % 10); 
+}
+
