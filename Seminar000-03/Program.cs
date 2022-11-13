399d1a46842a3eb5f308311995183311fee474e0
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-13 20:54:46 +0300

    Поиск целых чисел от -N  до N

---
+++ Seminar000-03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = -number1;
+
+while (number2 <= number1)
+{
+    Console.WriteLine(number2);
+    number2 += 1;
+}
